4ce85eeec1373530aaa09bea2cc5815da6bbe9e1
Lorismelik <lorismelik@yandex.ru>
2018-10-30 21:13:18 +0300

    without remove method

--- Task2_1.hs
+++ Task2_1.hs
@@ -1,43 +1,87 @@
 module Task2_1 where
 
 import Todo(todo)
+import Prelude hiding (lookup)
 
 -- Ассоциативный массив на основе бинарного дерева поиска
 -- Ключи - Integer, значения - произвольного типа
-data TreeMap v = ChangeMe 
+data TreeMap v = Nil 
+                | Node Integer v (TreeMap v) (TreeMap v) Integer
+                deriving(Show, Eq)
+
+
 
 -- Пустое дерево
 emptyTree :: TreeMap v
-emptyTree = todo
+emptyTree = Nil
 
 -- Содержится ли заданный ключ в дереве?
 contains :: TreeMap v -> Integer -> Bool
-contains t k = todo
+contains t k = case(t) of 
+    Nil -> False
+    Node key _ _ _ _| key == k -> True
+    Node key _ l _ _| key > k -> contains l k
+    Node key _ _ r _| key < k -> contains r k
 
 -- Значение для заданного ключа
 lookup :: Integer -> TreeMap v -> v
-lookup k t = todo
+lookup k t = case(t) of 
+    Nil -> error "Key not found"
+    Node key val _ _ _| key == k -> val
+    Node key _ l _ _| key > k -> lookup k l 
+    Node key _ _ r _| key < k -> lookup k r 
 
 -- Вставка пары (ключ, значение) в дерево
 insert :: (Integer, v) -> TreeMap v -> TreeMap v
-insert (k, v) t = todo
+insert (k, v) t = case(t) of 
+    Nil -> Node k v Nil Nil 1
+    Node key _ _ _ _| key == k -> error "Duplicate key"
+    Node key val l r s| key > k -> Node key val (insert (k, v) l) r (s + 1)  
+    Node key val l r s| key < k -> Node key val l (insert (k, v) r) (s + 1) 
 
 -- Удаление элемента по ключу
 remove :: Integer -> TreeMap v -> TreeMap v
-remove i t = todo
+remove i t = case(t) of 
+    Nil -> error "Key not found"
+
 
 -- Поиск ближайшего снизу ключа относительно заданного
 nearestLE :: Integer -> TreeMap v -> (Integer, v)
-nearestLE i t = todo
+nearestLE i t = case(t) of 
+    Nil -> error "Could not find"
+    Node key val l r _ | key == i -> (key, val)
+                       | key > i -> nearestLE i l
+                       | key < i -> case(r) of
+                          Node k v _ _ _| k == i -> (k, v)
+                          Node k v _ _ _| k < i -> nearestLE i r
+                          otherwise -> (key, val)
 
 -- Построение дерева из списка пар
 treeFromList :: [(Integer, v)] -> TreeMap v
-treeFromList lst = todo
+treeFromList lst = foldr insert Nil lst
 
 -- Построение списка пар из дерева
 listFromTree :: TreeMap v -> [(Integer, v)]
-listFromTree t = todo
+listFromTree t = case(t) of
+    Nil -> []
+    Node k v l r _ -> listFromTree l ++ [(k, v)] ++ listFromTree r
+
+
 
 -- Поиск k-той порядковой статистики дерева 
 kMean :: Integer -> TreeMap v -> (Integer, v)
-kMean i t = todo
+kMean i t = case (t) of
+    Nil -> error ":("
+    Node k v l r s | (sizeOf l) == i -> (k, v)
+                   | (sizeOf l) > i ->  kMean i l
+                   | (sizeOf l) < i ->  kMean (i - (sizeOf l) - 1) r
+    
+
+
+sizeOf :: TreeMap v -> Integer
+sizeOf (Nil) = 0
+sizeOf (Node _ _ _ _ s) = s
+
+nums = [(8, 0),(6, 0),(4, 0),(1, 0),(7, 0),(3, 0),(5, 0)]
+
+tree = treeFromList nums
--- Task2_2.hs
+++ Task2_2.hs
@@ -6,13 +6,22 @@ import Prelude hiding (foldl, foldr, unfoldr, map, concatMap,
     filter, maxBy, minBy, reverse, sum, product, elem)
 
 foldl :: (b -> a -> b) -> b -> [a] -> b
-foldl = todo
+foldl f b lst = case lst of
+        [] -> b
+        (h:t) -> foldl f (f b h) t
 
 foldr :: (a -> b -> b) -> b -> [a] -> b
-foldr = todo
+foldr f b lst =
+    case lst of
+        [] -> b
+        (h:t) -> f h (foldr f b t)
 
 unfoldr :: (b -> Maybe (a, b)) -> b -> [a]
-unfoldr = todo
+unfoldr f b = case f b of
+    Just (a, b') -> a : unfoldr f b'
+    Nothing -> []
+
+lst = [4, 5, 3, 8, 1]
 
 -- Сумма всех элементов списка (пример)
 sum :: [Integer] -> Integer
@@ -24,37 +33,46 @@ reverse lst = foldl f [] lst where f t h = h:t
 
 -- Отображение элементов списка
 map :: (a -> b) -> [a] -> [b]
-map = todo
+map f = foldr (\a acc -> (f a) : acc) []
 
 -- Произведение всех элементов списка
 product :: [Integer] -> Integer
-product = todo
+product = foldr (*) 1
 
 -- Выделение из списка Maybe всех существующих значений
 catMaybes :: [Maybe a] -> [a]
-catMaybes = todo
+catMaybes = foldr (\ item lst -> case item of 
+    Just item -> item:lst
+    Nothing -> lst) []
 
 -- Диагональ матрицы
 diagonal :: [[a]] -> [a]
-diagonal = todo
+diagonal x = zipWith (!!) x [0..]
+
+matrix = [[1, 2, 3],
+          [4, 5, 6],
+          [7, 8, 9]]
 
 -- Фильтр для всех элементов, не соответствующих предикату
 filterNot :: (a -> Bool) -> [a] -> [a]
-filterNot = todo
+filterNot f = foldr (\a acc -> if f a then acc else a : acc) []
 
 -- Поиск элемента в списке
 elem :: (Eq a) => a -> [a] -> Bool
-elem = todo
+elem x lst = foldr (||) False (map (\item -> item == x) lst)
 
 -- Список чисел в диапазоне [from, to) с шагом step
 rangeTo :: Integer -> Integer -> Integer -> [Integer]
-rangeTo from to step = todo
+rangeTo from to step   | to < from = error ":("
+                       | to - from < step = [from]
+                       | otherwise = unfoldr (\ x -> if x + step > to then Nothing else Just(x, x + step)) from
 
 -- Конкатенация двух списков
 append :: [a] -> [a] -> [a]
-append = todo
+append lst1 lst2 = foldr (:) lst1 lst2 
 
 -- Разбиение списка lst на куски размером n
 -- (последний кусок может быть меньше)
 groups :: [a] -> Integer -> [[a]]
-groups lst n = todo
+groups lst n = unfoldr (\ x -> if null x  then Nothing else Just(take (fromIntegral n) x , drop (fromIntegral n) x)) lst
+ 
