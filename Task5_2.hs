3deccce0e6f6fa02cd840fec0fa4777e2aa8c940
Timur Erniyazov <terniyazov@hazeltree.com>
2018-12-15 20:46:57 +0300

    complete 5_2

--- Task5_2.hs
+++ Task5_2.hs
@@ -2,6 +2,7 @@ module Task5_2 where
 
 import Todo(todo)
 import Data.Ratio
+import Prelude hiding (round)
 
 data Stream a = Cons {
                     shead :: a,
@@ -31,8 +32,28 @@ instance Monad Stream where
     return x = srepeat x
     ss >>= f = sflatten (f <$> ss)
 
+
 sinPrecisions :: Double -> Stream Double
-sinPrecisions = todo
+sinPrecisions x = sinPrecisions' (round x (2.0 * pi)) 0 0
+
+sinFun :: Double -> Double -> Double -> Double
+sinFun x n s = s + (-1) ** n * x ** p / product[1..p]  where 
+    p = (2 * n + 1)
+
+sinPrecisions' :: Double -> Double -> Double -> Stream Double
+sinPrecisions' x n s = Cons (sinFun x n s) $ sinPrecisions' x (n + 1) (sinFun x n s)
+
+eFun :: Rational -> Rational -> Rational
+eFun n s = s + 1 / product[1..n]
+
+ePrecisions' :: Rational -> Rational -> Stream Rational
+ePrecisions' n s = Cons (eFun n s)  $ ePrecisions' (n + 1) (eFun n s)
 
 ePrecisions :: Stream Rational
-ePrecisions = todo
+ePrecisions = ePrecisions' 0 0
+
+round :: Double -> Double -> Double
+round x y = x - (fromIntegral f) * y where 
+    f = floor (x / y)
+
+
